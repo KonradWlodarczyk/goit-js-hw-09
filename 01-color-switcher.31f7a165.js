3a1827a326b29f579122c7c59e429127373bd0ca
KonradWlodarczyk <105551464+KonradWlodarczyk@users.noreply.github.com>
2022-11-01 13:08:48 +0000

    Deploying to gh-pages from @ KonradWlodarczyk/goit-js-hw-09@0c392b06936e7f5d840873686762399139ab1a48 🚀

---
+++ 01-color-switcher.31f7a165.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let d;e.setAttribute("disabled","");t.addEventListener("click",(()=>{t.setAttribute("disabled",""),e.removeAttribute("disabled",""),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{t.removeAttribute("disabled",""),e.setAttribute("disabled",""),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.31f7a165.js.map
